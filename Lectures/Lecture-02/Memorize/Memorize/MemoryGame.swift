8a936ff1be210d1c6aa3dc542905acd2fd38c543
maunesh <rhythm.guitar.music@gmail.com>
2020-05-21 16:39:39 -0700

    lecture 2

---
+++ Lectures/Lecture-02/Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,35 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Maunesh Ahir on 5/21/20.
+//  Copyright © 2020 Maunesh Ahir. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1)) // append 2 cards, since we need a pair
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+    
+}
